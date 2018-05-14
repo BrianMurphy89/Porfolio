3b5066ad6d2dc4ec89f4f5f45af8e2e79b4696f9
Brian R Murphy <BrianRMurphy89@gmail.com>
2018-05-13 23:08:18 -0400

    rough draft of portfolio

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+const app = angular.module('portfolioApp', ['ngRoute']);
+
+app.controller('mainController', ['$http', function($http){
+
+    this.currentPage = 'home'
+
+    this.showHome = () => {
+        this.currentPage = 'home'
+    }
+
+    this.showAbout = () => {
+        this.currentPage = 'about'
+    }
+
+    this.showProjects = () => {
+        this.currentPage = 'projects'
+    }
+
+    this.showResume = () => {
+        this.currentPage = 'resume'
+    }
+}])
